0249c348e056a8ebb87a96e45e81da2282de9314
Hakan Sahin <hakan.sahin994@gmail.com>
2024-06-04 14:44:13 +0200

    Updated http request

--- Bank-Desktop-UI/Bank-Desktop-UI/Http Request/HttpRequests.cs
+++ Bank-Desktop-UI/Bank-Desktop-UI/Http Request/HttpRequests.cs
@@ -1,5 +1,7 @@
-﻿using System.Net.Http;
+﻿using System.Net;
+using System.Net.Http;
 using System.Net.Http.Json;
+using System.Windows;
 
 namespace Bank_Desktop_UI.Http_Request
 {
@@ -14,34 +16,57 @@ namespace Bank_Desktop_UI.Http_Request
         public static async Task<ReturnObject?> SendHttpGetRequest<ReturnObject>(Enum Controller, string Path)
             where ReturnObject : class
         {
-            try
-            {
-                var innerPath = $"{GetEndpointFromEnum(Controller)}/{Path}";
-                var result = await HttpClient.GetFromJsonAsync<ReturnObject>(innerPath);
-                return result;
-            }
-            catch (Exception ex)
-            {
-                //add error handling
-                return null;
-            }
+            var innerPath = $"{GetEndpointFromEnum(Controller)}/{Path}";
+            return await ExecuteWithExceptionHandling(() => SendHttpRequest<ReturnObject>(HttpMethod.Get, innerPath));
         }
 
         public static async Task<ReturnObject?> SendHttpPostRequest<ReturnObject>(Enum Controller, string Path, object PostObject)
             where ReturnObject : class
         {
+            var innerPath = $"{GetEndpointFromEnum(Controller)}/{Path}";
+            return await ExecuteWithExceptionHandling(() => SendHttpRequest<ReturnObject>(HttpMethod.Post, innerPath, PostObject));
+        }
+
+        private static async Task<ReturnObject?> ExecuteWithExceptionHandling<ReturnObject>(Func<Task<ReturnObject?>> httpRequestFunc)
+            where ReturnObject : class
+        {
             try
             {
-                var innerPath = $"{GetEndpointFromEnum(Controller)}/{Path}";
-                var responseMessage = await HttpClient.PostAsJsonAsync(innerPath, PostObject);
-                responseMessage.EnsureSuccessStatusCode();
-                var result = await responseMessage.Content.ReadFromJsonAsync<ReturnObject>();
-                return result;
+                return await httpRequestFunc();
             }
-            catch(Exception ex)
+            catch (HttpRequestException ex) when (ex.StatusCode == HttpStatusCode.BadRequest)
             {
-                return null;
+                // Log error
             }
+            catch 
+            {
+                // log general error
+                MessageBox.Show("Connection to server could not be established");
+            }
+            return null;
+        }
+
+        private static async Task<ReturnObject?> SendHttpRequest<ReturnObject>(HttpMethod method, string innerPath, object? content = null)
+            where ReturnObject : class
+        {
+            HttpResponseMessage response;
+
+            if (method == HttpMethod.Get)
+            {
+                response = await HttpClient.GetAsync(innerPath);
+            }
+            else if (method == HttpMethod.Post && content != null)
+            {
+                response = await HttpClient.PostAsJsonAsync(innerPath, content);
+            }
+            else
+            {
+                throw new ArgumentException("Unsupported HTTP method or missing content for POST request.");
+            }
+
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadFromJsonAsync<ReturnObject>();
+            return result;
         }
 
         public enum Endpoints
@@ -69,7 +94,7 @@ namespace Bank_Desktop_UI.Http_Request
                 case Endpoints.Transaction:
                     return baseApiPath + "Transaction";
                 default:
-                    throw new ArgumentException("Invalid data");
+                    throw new NotImplementedException("Endpoint not implemented for Enum");
             }
         }
     }
