940f03a5268c5331bf5fd2c22f5748c0e87be321
Hakan Sahin <hakan.sahin994@gmail.com>
2024-07-06 17:48:13 +0200

    Updated dropdown component Added psudo code onto transfers

---
+++ Bank-Web-App/Bank-Web-App/Pages/Shared/Dropdown/DropdownItem.cs
@@ -0,0 +1,15 @@
+﻿namespace Bank_Web_App.Pages.Shared
+{
+
+    public class DropdownItem
+    {
+        public DropdownItem(string text, int value)
+        {
+            Text = text;
+            Value = value;
+        }
+
+        public string Text { get; set; }
+        public int Value { get; set; }
+    }
+}
