6d7a48a3edef322669420736644a84f9a5b9e6b0
Hakan Sahin <hakan.sahin994@gmail.com>
2024-07-08 21:01:27 +0200

    added binding and get data from API endpoint

---
+++ Bank-Web-App/Bank-Shared-Classes/Models/TransactionResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Xml.Linq;
+
+namespace Bank_Shared_Classes.Models
+{
+    public class TransactionResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Timestamp { get; set; }
+        public bool Sender { get; set; }
+        public double Amount { get; set; }
+        public int FromAccountId { get; set; }
+        public int ToAccountId { get; set; }
+        public string SenderName { get; set; }
+    }
+}
